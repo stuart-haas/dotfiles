33ee2f9b0bd294d00e33cd55c18ccf50d6a90db1
Stuart Haas <stuarthaas@Admins-MacBook-Pro.local>
2020-09-10 15:51:46 -0400

    Escape special characters for zshrc

--- init/zsh.sh
+++ init/zsh.sh
@@ -24,7 +24,7 @@ echo "autoload -Uz compinit" >> ~/.zshrc
 
 echo "\n# Cache completion if nothing changed - faster startup time" >> ~/.zshrc
 echo "typeset -i updated_at=\$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
-if [ $(date +'%j') != $updated_at ]; then
+if [ \$(date +'%j') != \$updated_at ]; then
   compinit -i
 else
   compinit -C -i
