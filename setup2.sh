84648755bc25ce0e997ed39487f8fd4ad0732559
Stuart Haas <stuarthaas@Admins-MacBook-Pro.local>
2020-09-10 15:47:31 -0400

    Uncomment setup2

--- setup2.sh
+++ setup2.sh
@@ -1,16 +1,16 @@
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}")"  && pwd)"
 
 # Install Yarn
-#curl -o- -L https://yarnpkg.com/install.sh | bash
+curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # Install global npm packages
-#source $DOTFILES/init/npm.sh
+source $DOTFILES/init/npm.sh
 
 # Install Ruby Gems
-#source $DOTFILES/init/gem.sh
+source $DOTFILES/init/gem.sh
 
 # Configure zsh
 source $DOTFILES/init/zsh.sh
 
 # Configure hyper.js
-#cp -v $DOTFILES/templates/.hyper.js ~/.hyper.js
+cp -v $DOTFILES/templates/.hyper.js ~/.hyper.js
